907bb2027aa4216ceed2dd87d1b6171f5b8cbb86
JasonSung <jason870724@gmail.com>
2024-04-26 18:12:57 +0800

    0426stock-out

--- StockOut.py
+++ StockOut.py
@@ -1,117 +1,250 @@
-import requests , json
+import requests , json , re , time
 from GlobalVar import *
 import math
-TestEnv = 'staging'
+TestEnv = 'dev'
+WMStoken = 'eyJhbGciOiJSUzI1NiJ9.eyJST0xFX1dNU19MRUFERVIiOnRydWUsIlJPTEVfV0hfRVJST1JfQ0hFQ0siOnRydWUsIlJPTEVfV01TX01HVCI6dHJ1ZSwidXNlclV1aWQiOiI5MTk2MjcwOC01Njg3LTQ5OWItYjgxZC01N2Y0MmUyODQ1MTMiLCJST0xFX1dNU19BRE1JTiI6dHJ1ZSwiUk9MRV9BRE1JTiI6dHJ1ZSwiUk9MRV9XSF9QQUNLIjp0cnVlLCJST0xFX05PQyI6dHJ1ZSwidXNlcm5hbWUiOiJUWVdNUy5UZXN0QWRtaW4xIiwic3ViIjoiVFlfTVdNU19TU08iLCJpYXQiOjE3MTQxMjI0NjMsImV4cCI6MTcxNDE0MDQ2M30.LOlKkasewB5QmdkLfMu6jqwOEPcSPT85cGADDW7sdaWr4uWZtIUO1zEzVgiuWk3pV_3MfKuJbWMO9tgLnfFeCViQH6QNZ88ieXAywU_KxnWSO7ZKbtN-OAbb6EcNhMvjOuySoCjjvqkpcR6vysi49EwIU-uzDjifN5h6l8gacEHtaGKZ7QDo-ARUdBj0-2d-bfi_GGOi4CDJA8dVffjiNhZpOZpSt6rtiG-RERD8hmgXszo-KEohajRMvgHHBYgL7vMIi4_DwClXO5Ds0JfNX0_12QCWySOCo7N1ayBCxOHUV9T4NfyC725ZXIi8Q7QE9MLJJzOe2cMhMhzXvyZ7Gg'
+if WMStoken == "" :
+    conn.execute("SELECT WMStoken FROM `3PL_Var_Table` WHERE ID = 1")
+    WMStokenResult = cur.fetchone()
+    WMStoken = WMStokenResult[0]
+    conn.commit()
+
 def __StockOutAPI__(BookingNumber):
+    Consolidation = __ConsolidationTaskHandle__(BookingNumber)
+    time.sleep(2)
+    CycleCount = __CycleCount__(BookingNumber)
+    WeigthCheck = __WeigthCheck__(BookingNumber)
+    if CycleCount == False and WeigthCheck == False :
+        print("Stock-out flow error")
+    elif CycleCount == True or WeigthCheck == True :
+        print("Please check stock-out status , should change to 'ready'")
+
+
+
+def __GetBookingInfo__(BookingNumber):
     GetStockOutInfoURL = f'https://mwms-whtsy-{TestEnv.lower()}.hkmpcl.com.hk/hktv_ty_mwms/cms/tote/booking_job/tote_record?bookingType=Stock+Out&bookingNo={BookingNumber}&pageNo=1&pageSize=100'
-    # conn.execute("SELECT WMStoken FROM `3PL_Var_Table` WHERE ID = 1")
-    # WMStokenResult = cur.fetchone()
-    # WMStoken = WMStokenResult[0]
-    # conn.commit()
-    WMStoken = 'eyJhbGciOiJSUzI1NiJ9.eyJST0xFX1dNU19MRUFERVIiOnRydWUsIlJPTEVfV0hfRVJST1JfQ0hFQ0siOnRydWUsIlJPTEVfV01TX01HVCI6dHJ1ZSwidXNlclV1aWQiOiI5YzNkOTUxYy0zOTRjLTQxNGEtODMxNi00OTZhZDM0ODAyZWEiLCJST0xFX1dNU19BRE1JTiI6dHJ1ZSwiUk9MRV9BRE1JTiI6dHJ1ZSwiUk9MRV9XSF9QQUNLIjp0cnVlLCJST0xFX05PQyI6dHJ1ZSwidXNlcm5hbWUiOiJUWVdNUy5UZXN0QWRtaW4xIiwic3ViIjoiVFlfTVdNU19TU08iLCJpYXQiOjE3MTQwNDMxMjEsImV4cCI6MTcxNDA2MTEyMX0.cAkO4h59WYb1MgzAiEvCE19FVyZ6-6tsUaIOTQhKo-N7qEFYemHQDeOYzFORw1Y4yV_41JCXo6tvlP6683tutQHXWL9zwcVynzcQbkMc1564uwAVyHjlGS5noKpfzWVcQZXs5Di7s1lGQcNy1fY4SbQCqLpS_Iv_l7MFmslbFQ9crT5YB9WfREY0CEw-LKY8pdcn1goEgrZ7Xdg9KCLoLj60IdIalabbEm2EmEeJQcg6s7QoCBDtFzrt73GV_mPy2-dhV9xQVkP09KsxD_9K_-U5cAIaAC5iXoEGRcEZht5mniuknrMo5P1-ADf18QO6M2JJ6ikFDzYx4xf9Gmzq3g'
     WMSheaders = {'Authorization' : f'Bearer {WMStoken}'}
     BookingInfoResponse = requests.get(GetStockOutInfoURL,headers=WMSheaders).json()
     BatchQTY = BookingInfoResponse['data']['pagination']['totalElements']
     print(f"Stock-out {BatchQTY} batch")
     BatchDict = {}
-    TaskList = []
     if BatchQTY <= 100 :
         BookingInfoList = BookingInfoResponse['data']['bookingRecords']
         for batch in BookingInfoList :
             BatchDict[batch['batchId']] = batch
     print(BatchDict)
+    return BatchDict
+            
+def __ConsolidationTaskHandle__(BookingNumber):
     GetTaskNoURL = f'https://mwms-whtsy-{TestEnv.lower()}.hkmpcl.com.hk/hktv_ty_mwms/task_number/get_task_number?serviceType=STOCK_OUT_CONSOLIDATION&bookingNo={BookingNumber}'
     TaskNoResponse = requests.get(GetTaskNoURL).json()
-    print(TaskNoResponse['resultStatus'])
+    print("Consolidation Get Task Number : " + TaskNoResponse['resultStatus'])
+    SKUdict = __GetBookingInfo__(BookingNumber)
     if TaskNoResponse['resultStatus'] == "SUCCESS" :
         TaskList = TaskNoResponse['responseData']
-    for task in TaskList :
-        __ConsolidationTaskHandle__(task , BatchDict)
-            
+        for Task in TaskList :
+            TaskNo = Task['taskNo']
+            Compartment = Task['batches'][0]['fromTote']
+            if Compartment.startswith("14"):
+                CompartmentType = "TY14"
+                CompartmentQty = 4
+            elif Compartment.startswith("12"):
+                CompartmentType = "TY12"
+                CompartmentQty = 2
+            else :
+                print("Please check consolidation task , occrus error")
 
-
-def __ConsolidationTaskHandle__(Task , SKUinfo):
-    TaskNo = Task['taskNo']
-    Compartment = Task['batches'][0]['fromTote']
-    if Compartment.startswith("14"):
-        CompartmentType = "TY14"
-        CompartmentQty = 4
-    elif Compartment.startswith("12"):
-        CompartmentType = "TY12"
-        CompartmentQty = 2
+            TotesQty = math.ceil(len(Task['batches'])/CompartmentQty)
+            TotesList = __GetTotes__(CompartmentType,TotesQty)
+            M5121Totaldata = []
+            j = 0 #計算空箱
+            BatchesList = Task['batches']
+            for i in range(len(Task['batches'])) : # 迴圈處理任務中的每個箱子
+                BatchId = BatchesList[i]['batchId']
+                FromTote = BatchesList[i]['fromTote']
+                FromCells = BatchesList[i]['fromComp']
+                Ean = SKUdict[BatchId]['ean']
+                UUID = SKUdict[BatchId]['uuid']
+                QTY = SKUdict[BatchId]['qty']
+                M5121data = {
+                            "taskNo": TaskNo,
+                            "isNormalComplete": True,
+                            "isLast": i == len(BatchesList) - 1 ,
+                            "stationKey": "test_7f036537fed7",
+                            "operatorId": "test_776ab1b1b4d2",
+                            "detail": [
+                                {
+                                "uId": UUID,
+                                "eanCode": Ean,
+                                "actualEanCode": Ean,
+                                "isLock": False,
+                                "fromCellsCode": FromCells,
+                                "toCellsCode": TotesList[j]['count'],
+                                "qty": QTY,
+                                "fromErrorCode": "",
+                                "fromRemarks": "",
+                                "toErrorCode": "",
+                                "toRemarks": ""
+                                }
+                            ],
+                            "fromContainerCode": FromTote,
+                            "toContainerCode": TotesList[j]['ToteCode'],
+                            "errorCode": "",
+                            "remarks": ""
+                            }
+                M5121Totaldata.append(M5121data)
+                TotesList[j]['count'] += 1
+                if TotesList[j]['count'] > CompartmentQty  and j <= TotesQty - 1:
+                    j += 1
+            M5121Body = json.dumps({
+                "msgTime": "test_23b549b76fda",
+                "msgId": "test_7817ccb87aba",
+                "data": M5121Totaldata
+                })
+            APIheaders = {"Content-Type" : "application/json"}
+            M5121URL = f'https://mwms-whtsy-{TestEnv.lower()}.hkmpcl.com.hk/hktv_ty_mwms/wcs/consolidation_complete_report'
+            M5121Response = requests.post(M5121URL,data=M5121Body,headers=APIheaders).json()
+            print(M5121Body)
+            print(M5121Response)
+        print("Stock-out consolidation completed")
+        time.sleep(2)
+        return True
     else :
-        print("Please check consolidation task , occrus error")
-
-    TotesQty = math.ceil(len(Task['batches'])/CompartmentQty)
-    TotesList = __GetTotes__(CompartmentType,TotesQty)
-
-    M5121Totaldata = []
-    j = 0 #計算空箱
-    BatchesList = Task['batches']
-    for i in range(len(Task['batches'])) : # 迴圈處理任務中的每個箱子
-        if TotesList[j][1] == CompartmentQty :
-            j += 1
-        BatchId = BatchesList[i]['batchId']
-        FromTote = BatchesList[i]['fromTote']
-        FromCells = BatchesList[i]['fromComp']
-        Ean = SKUinfo[BatchId]['ean']
-        UUID = SKUinfo[BatchId]['uuid']
-        QTY = SKUinfo[BatchId]['qty']
-        M5121data = {
-                    "taskNo": TaskNo,
-                    "isNormalComplete": True,
-                    "isLast": i == len(BatchesList) - 1 ,
-                    "stationKey": "test_7f036537fed7",
-                    "operatorId": "test_776ab1b1b4d2",
-                    "detail": [
-                        {
-                        "uId": UUID,
-                        "eanCode": Ean,
-                        "actualEanCode": Ean,
-                        "isLock": False,
-                        "fromCellsCode": FromCells,
-                        "toCellsCode": TotesList[j][1],
-                        "qty": QTY,
-                        "fromErrorCode": "",
-                        "fromRemarks": "",
-                        "toErrorCode": "",
-                        "toRemarks": ""
-                        }
-                    ],
-                    "fromContainerCode": FromTote,
-                    "toContainerCode": TotesList[j][0],
-                    "errorCode": "",
-                    "remarks": ""
-                    }
-        M5121Totaldata.append(M5121data)
-    M5121Body = json.dumps({
-        "msgTime": "test_23b549b76fda",
-        "msgId": "test_7817ccb87aba",
-        "data": M5121Totaldata
-        })
-    APIheaders = {"Content-Type" : "application/json"}
-    M5121URL = f'https://mwms-whtsy-{TestEnv.lower()}.hkmpcl.com.hk/hktv_ty_mwms/wcs/consolidation_complete_report'
-    M5121Response = requests.post(M5121URL,data=M5121Body,headers=APIheaders).json()
-    print(M5121Body)
-    print(M5121Response)
+        print("Maybe shouldn't consolidate or occur dirty data error.")
+        return False
     
         
 def __GetTotes__(CompartmentType,TotesQty):
     # 獲取Tote , 條件(Available for rent , In System , TY3F)
-    # conn.execute("SELECT WMStoken FROM `3PL_Var_Table` WHERE ID = 1")
-    # WMStokenResult = cur.fetchone()
-    # WMStoken = WMStokenResult[0]
-    # conn.commit()
-    WMStoken = 'eyJhbGciOiJSUzI1NiJ9.eyJST0xFX1dNU19MRUFERVIiOnRydWUsIlJPTEVfV0hfRVJST1JfQ0hFQ0siOnRydWUsIlJPTEVfV01TX01HVCI6dHJ1ZSwidXNlclV1aWQiOiI5YzNkOTUxYy0zOTRjLTQxNGEtODMxNi00OTZhZDM0ODAyZWEiLCJST0xFX1dNU19BRE1JTiI6dHJ1ZSwiUk9MRV9BRE1JTiI6dHJ1ZSwiUk9MRV9XSF9QQUNLIjp0cnVlLCJST0xFX05PQyI6dHJ1ZSwidXNlcm5hbWUiOiJUWVdNUy5UZXN0QWRtaW4xIiwic3ViIjoiVFlfTVdNU19TU08iLCJpYXQiOjE3MTQwNDMxMjEsImV4cCI6MTcxNDA2MTEyMX0.cAkO4h59WYb1MgzAiEvCE19FVyZ6-6tsUaIOTQhKo-N7qEFYemHQDeOYzFORw1Y4yV_41JCXo6tvlP6683tutQHXWL9zwcVynzcQbkMc1564uwAVyHjlGS5noKpfzWVcQZXs5Di7s1lGQcNy1fY4SbQCqLpS_Iv_l7MFmslbFQ9crT5YB9WfREY0CEw-LKY8pdcn1goEgrZ7Xdg9KCLoLj60IdIalabbEm2EmEeJQcg6s7QoCBDtFzrt73GV_mPy2-dhV9xQVkP09KsxD_9K_-U5cAIaAC5iXoEGRcEZht5mniuknrMo5P1-ADf18QO6M2JJ6ikFDzYx4xf9Gmzq3g'
     WMSheaders = {'Authorization' : f'Bearer {WMStoken}'}
     GetTotesURL = f'https://mwms-whtsy-{TestEnv.lower()}.hkmpcl.com.hk/hktv_ty_mwms/cms/inventory_tote?pageNo=5&pageSize=10&sort=toteCode:asc&status=Available+for+rent&warehouseCode=TY3F&toteType={CompartmentType}&locationType=In+System'
     GetToteResponse = requests.get(GetTotesURL,headers=WMSheaders).json()
     TotesRecord = GetToteResponse['data']['totes']
     ToteList = []
     for i in range(TotesQty) :
-        ToteList.append((TotesRecord[i]['toteCode'],0))
+        ToteList.append({"ToteCode" : TotesRecord[i]['toteCode'],"count": 1})
+    #該作法只能最多抓一頁的10個箱子，若需要抓超過10個箱子會導致後面邏輯錯誤
     print(ToteList)
     return ToteList
 
+def __GetSKUinfo__(uuid):
+    URL = f'https://mwms-whtsy-{TestEnv.lower()}.hkmpcl.com.hk/hktv_ty_mwms/cms/sku_inventory?pageNo=1&pageSize=10&skuUuid={uuid}&sort=skuUuid:desc'
+    WMSheaders = {'Authorization' : f'Bearer {WMStoken}'}
+    Response = requests.get(url=URL,headers=WMSheaders).json()
+    SKUinfo = Response['data']['skuInventories'][0]
+    return SKUinfo
 
-__StockOutAPI__("SOTY3F00000651")
+def __CycleCount__(BookingNumber):
+    SKUdict = __GetBookingInfo__(BookingNumber)
+    GetTaskNoURL = f'https://mwms-whtsy-{TestEnv.lower()}.hkmpcl.com.hk/hktv_ty_mwms/task_number/get_task_number?serviceType=STOCK_OUT_CYCLE&bookingNo={BookingNumber}'
+    TaskNoResponse = requests.get(GetTaskNoURL).json()
+    print("Cycle Count Get Task Number : " + TaskNoResponse['resultStatus'])
+    if TaskNoResponse['resultStatus'] == "SUCCESS" :
+        TaskList = TaskNoResponse['responseData']
+        TaskDict = {}
+        for batches in TaskList:
+            if batches['taskNo'] not in TaskDict:
+                TaskDict[batches['taskNo']] = [batches['batchId']]
+            else :
+                TaskDict[batches['taskNo']].append(batches['batchId'])
+        for task , batchid in TaskDict.items() :
+            M5108TotalDetail = []
+            # 處理同一個隔層內有兩個Batch ID 的髒資料
+            try:
+                TOTE = SKUdict[batchid[0]]['toteNo']
+            except IndexError:
+                print("Dirty data")
+                return
+            except KeyError:
+                print(f"Dirty data , have multiple batchid with same compartment {batchid[0]}")
+                
+                return
+            for batch in batchid :
+                Compartment = SKUdict[batch]['compartment']
+                if Compartment == "A" : CellsCode = 1 
+                elif Compartment == "B" : CellsCode = 2
+                elif Compartment == "C" : CellsCode = 3
+                elif Compartment == "D" : CellsCode = 4
+                UUID = SKUdict[batch]['uuid']
+                QTY = SKUdict[batch]['qty']
+                EAN = SKUdict[batch]['ean']
+                SKUinfo = __GetSKUinfo__(UUID)
+                length = SKUinfo['length']
+                width = SKUinfo['width']
+                height = SKUinfo['height']
+                SKU_weight = re.match(r"(\d+)", SKUinfo['skuWeight'])
+                weight = float(SKU_weight.group(1))
+                M5108Detail = {
+                                "cellsCode": CellsCode,
+                                "eanCode": EAN,
+                                "actualEanCode": EAN,
+                                "lotId":batch,
+                                "qty": QTY,
+                                "actualQty": QTY,
+                                "length": length,
+                                "width": width,
+                                "weight": weight,
+                                "height": height,
+                                "success": False,
+                                "errorCode": "",
+                                "remarks": ""
+                                }
+                M5108TotalDetail.append(M5108Detail)                 
+            M5108Body = json.dumps({
+                        "data": [
+                            {
+                            "taskNo": task,
+                            "containerCode": TOTE,
+                            "stationKey": "test_61763bae8367",
+                            "operatorId": "test_9c5e08a0562b",
+                            "detail": M5108TotalDetail
+                            }
+                        ],
+                        "msgTime": "test_58e0dd20cdeb",
+                        "msgId": "test_48044c1054aa"
+                        })
+            M5108URL = f'https://mwms-whtsy-{TestEnv.lower()}.hkmpcl.com.hk/hktv_ty_mwms/wcs/take_inventory_report'
+            APIheaders = {"Content-Type" : "application/json"}
+            M5108Response = requests.post(M5108URL,data=M5108Body,headers=APIheaders).json()
+            print(M5108Body)
+            print(M5108Response)
+        return True
+    else:
+        return False
+
+def __WeigthCheck__(BookingNumber):
+    GetTaskNoURL = f'https://mwms-whtsy-{TestEnv.lower()}.hkmpcl.com.hk/hktv_ty_mwms/task_number/get_task_number?serviceType=STOCK_OUT_WEIGHT&bookingNo={BookingNumber}'
+    TaskNoResponse = requests.get(GetTaskNoURL).json()
+    print("Weigth Check Task Number : " + TaskNoResponse['resultStatus'])
+    if TaskNoResponse['resultStatus'] == "SUCCESS" :
+        TaskList = TaskNoResponse['responseData']
+        for task in TaskList :
+            TaskNo = task['taskNo']
+            ToteCode = task['toteCode']
+            CorrectWeigth = float((task['weightUpperLimit'] + task['weightLowerLimit'])/2)
+            M5112Body = json.dumps({
+                        "msgTime": "2021-09-01T19:02:33.597+08:00",
+                        "msgId": "e4ef9fee-d6de-4bcc-9a90-cb1e091a6092",
+                        "data": [
+                            {
+                                "taskNo": TaskNo,
+                                "containerCode": ToteCode,  
+                                "balanceCode": "1332",
+                                "weight": CorrectWeigth
+                            }
+                        ]
+                    })
+            M5112URL = f'https://mwms-whtsy-{TestEnv.lower()}.hkmpcl.com.hk/hktv_ty_mwms/wcs/tote_weight_report'
+            APIheaders = {"Content-Type" : "application/json"}
+            M5112Response = requests.post(M5112URL,data=M5112Body,headers=APIheaders).json()
+            print(M5112Body)
+            print(M5112Response)
+            print("Weigth check completed")
+    else:
+        return False
+
+
+
+__StockOutAPI__("SOTY3F00001036")
+# __CycleCount__('SOTY3F00000664')
+
