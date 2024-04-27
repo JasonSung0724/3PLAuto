c45d77b717e5f7f760ef11f88769a71284a24a9e
宋嘉勳 <117521081+JasonSung0724@users.noreply.github.com>
2024-04-28 00:07:04 +0800

    Delete 3PLAuto directory

--- 3PLAuto/GlobalVar.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('/Users/mac/Documents/GitHub/Jason-3PL/3PLAuto/3PL.db')
-cur = conn.cursor()
-
-TestEnv = 'dev'
-MMSAccount = 'cindy.yeh@shoalter.com'
-MMSPasword = 'Aa123456'
